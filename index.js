23933939ad88f5045d01d0d3d3b77165f9a6272e
Gatsbeau <jabriel0ne@gmail.com>
2023-10-29 03:40:55 -0400

    Added svg generator to exports; created filewrite function

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const inquirer = require('inquirer')
-const {writeFile} = require('fs').promises
-const shapes = require('./lib/shapes')
+const inquirer = require("inquirer");
+const { writeFile } = require("fs").promises;
+const generateSVG = require("./lib/shapes");
 
 const promptUser = () => {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter up to three(3) characters.'
-        },
-        {
-            type: 'input',
-            name: 'text-color',
-            message: "Enter a color for your text (ex. 'blue' or '0000FF')"
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: "What shape do you want your logo?",
-            choices: ['circle', 'triangle', 'square'],
-        },
-        {
-            type: 'input',
-            name: 'shape-color',
-            message: "Enter a color for your shape (ex. 'blue' or '0000FF')"
-        }
-    ])
-}
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three(3) characters.",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter a color for your text (ex. 'blue' or '0000FF')",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape do you want your logo?",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter a color for your shape (ex. 'blue' or '0000FF')",
+    },
+  ]);
+};
 
 function init() {
-    promptUser()
+  promptUser()
+    .then((answers) => writeFile("examples/sample.svg", generateSVG(answers)))
+    .then(() => console.log("Generated logo.svg"))
+    .catch((err) => console.log(err));
 }
+
+init()
